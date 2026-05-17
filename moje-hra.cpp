a12dc34a86a6aa83ca28a691335b6bf19add947b
Hajtol <patrikhajtol@gmail.com>
2026-05-17 19:56:45 +0200

    skutecny finalni opravny commit

--- moje-hra.cpp
+++ moje-hra.cpp
@@ -4,9 +4,8 @@
 using namespace std;
 
 int main() {
-
     string jmeno;
-    int hp = 10;
+    int hp = 15; // Zvýšený základ, abys přežil k vesnici
     int utok = 3;
     int zlato = 0;
     int volba;
@@ -26,7 +25,7 @@ int main() {
     }
 
     int potvoraHP = 8;
-    cout << "\n--- POTKAl JSI MONSTRUM (HP: " << potvoraHP << ") ---" << endl;
+    cout << "\n--- POTKAL JSI MONSTRUM (HP: " << potvoraHP << ") ---" << endl;
 
     while (potvoraHP > 0 && hp > 0) {
         cout << "Utocis na priseru!" << endl;
@@ -38,16 +37,20 @@ int main() {
             hp = hp - 2;
             cout << "Tvoje HP: " << hp << endl;
         }
+    } 
      
     if (hp > 0) {
+        cout << "\nPorazil jsi prvni priseru! Ziskavas 15 zlata." << endl;
+        zlato = zlato + 15;
+
         cout << "\nPo narocnem boji jsi dorazil do vesnice 'U Spateho Programatora'." << endl;
         cout << "Ve vesnici si muzes odpocinout a nakoupit." << endl;
         
         bool ve_vesnici = true;
         while (ve_vesnici) {
             cout << "\n--- VESNICE (Zlato: " << zlato << " | HP: " << hp << ") ---" << endl;
-            cout << "1 - Leceni (stoji 5 zlata)" << endl;
-            cout << "2 - Trenink utoku (stoji 10 zlata)" << endl;
+            cout << "1 - Leceni +5 HP (stoji 5 zlata)" << endl;
+            cout << "2 - Trenink utoku +2 (stoji 10 zlata)" << endl;
             cout << "3 - Odejit z vesnice" << endl;
             cout << "Vase volba: ";
             cin >> volba;
@@ -75,18 +78,17 @@ int main() {
                 cout << "Odchazis z vesnice vstric dalsimu dobrodruzstvi..." << endl;
             }
         }
-
    
-        cout << "\nUroven tve postavy se zvysila! Citit se silnejsi." << endl;
-        int maxHP = 20; 
+        cout << "\nUroven tve postavy se zvysila! Citis se silnejsi." << endl;
+        int maxHP = 35; // Vybalancované maximum zdraví
         hp = maxHP;
-        cout << "Novy level postavy. Max zivoty se zvysily na: " << maxHP << endl;
+        cout << "Novy level postavy. Zivoty se doplnily na: " << maxHP << endl;
         
-        cout << "\nAle pozor! V krovi susti dalsi monstrum..." << endl;
+        cout << "\nAle pozor! V krovi susti dalsi monstra..." << endl;
    
-    // SOUBOJ SE DVĚMA MONSTRY 
-        int vlk1_HP = 10;
-        int vlk2_HP = 10;
+        // SOUBOJ SE DVĚMA MONSTRY 
+        int vlk1_HP = 12;
+        int vlk2_HP = 12;
         cout << "Vyskocili na tebe dva vlci najednou!" << endl;
 
         while ((vlk1_HP > 0 || vlk2_HP > 0) && hp > 0) {
@@ -95,10 +97,10 @@ int main() {
 
             if (volba == 1 && vlk1_HP > 0) {
                 vlk1_HP = vlk1_HP - utok;
-                cout << "Zasahneš prvního vlka!" << endl;
+                cout << "Zasahnes prvniho vlka!" << endl;
             } else if (volba == 2 && vlk2_HP > 0) {
                 vlk2_HP = vlk2_HP - utok;
-                cout << "Zasáhneš druhého vlka!" << endl;
+                cout << "Zasahnes druheho vlka!" << endl;
             } else {
                 cout << "Ten uz je mrtvy nebo jsi minul!" << endl;
             }
@@ -116,11 +118,68 @@ int main() {
 
         if (hp > 0) {
             cout << "\nZvladl jsi to! Oba vlci jsou pryc." << endl;
-            zlato = zlato + 20;
-            cout << "Nasel jsi u nich 20 zlata. Celkem mas: " << zlato << endl;
-            cout << "Ted se musis pripravit na neco mnohem vetsiho..." << endl;
+            // Místo nesmyslného plného vyléčení dostaneš jen malou lékárničku
+            hp = hp + 10;
+            cout << "Nasel jsi u nich maly lektvar. Dopinil ti 10 HP. (Tvoje HP: " << hp << ")" << endl;
+            
+            // --- FINÁLNÍ BOSS: CHRONOBOT ---
+            cout << "\n=========================================" << endl;
+            cout << "   [ERROR] NARUSENI CASOPROSTORU!" << endl;
+            cout << "  *** VAROVANI: PRICHAZI CHRONOBOT *** " << endl;
+            cout << "=========================================" << endl;
+            
+            int bossHP = 40; 
+            int kolo = 1;
+            int posledniSkodaBosse = 0;
+
+            while (bossHP > 0 && hp > 0) {
+                cout << "\n--- KOLO " << kolo << " ---" << endl;
+                cout << "Utocis na Chronobota!" << endl;
+                bossHP = bossHP - utok;
+                cout << "Chronobotovi zbyva HP: " << bossHP << endl;
+
+                if (bossHP <= 0) {
+                    break; 
+                }
+
+                int skodaBosse = 0;
+                int vzor = kolo % 4; 
+
+                if (vzor == 1) { 
+                    cout << "Chronobot pouzil SLABY utok." << endl; 
+                    skodaBosse = 2; 
+                } else if (vzor == 2) { 
+                    cout << "Chronobot pouzil STREDNI utok." << endl; 
+                    skodaBosse = 4; 
+                } else if (vzor == 3) { 
+                    cout << "Chronobot pouzil SILNY utok." << endl; 
+                    skodaBosse = 6; 
+                } else { 
+                    cout << "!!! Chronobot pouzil KRITICKY utok !!!" << endl; 
+                    skodaBosse = 10; 
+                }
+
+                hp = hp - skodaBosse;
+                posledniSkodaBosse = skodaBosse; 
+
+                if (kolo % 5 == 0) {
+                    cout << "\nChronobot PRESKOCIL CAS a utoci znovu stejnou silou!" << endl;
+                    hp = hp - posledniSkodaBosse;
+                }
+
+                cout << "Tvoje zbyvajici HP: " << hp << endl;
+                kolo = kolo + 1;
+            }
+
+            if (hp > 0) {
+                cout << "\nGRATULUJEME! Porazil jsi Chronobota a zachranil casovou osu!" << endl;
+                cout << "=== VYHRAL JSI HRU ===" << endl;
+            } else {
+                cout << "\nChronobot te vymazal z historie. KONEC HRY." << endl;
+            }
+
         } else {
-            cout << "\nVlci te roztrhali. Hra končí." << endl;
+            cout << "\nVlci te roztrhali. Hra konci." << endl;
         }
     } 
 
